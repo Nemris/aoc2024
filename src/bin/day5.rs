6b4c10ade25bf5da9bd3ecc8b9b6ad404ac6880d
Nemris <nemris@protonmail.com>
2024-12-11 07:53:25 +0100

    Add solution for Day 5 part 1

---
+++ src/bin/day5.rs
@@ -0,0 +1,162 @@
+#![warn(clippy::pedantic)]
+
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::num::ParseIntError;
+use std::path::PathBuf;
+use std::slice::Iter;
+use std::str::FromStr;
+
+/// A printing rule.
+#[derive(Debug)]
+struct Rule {
+    /// This number must come before `y` in a valid `Update`.
+    x: u32,
+    /// This number must come after `x` in a valid `Update`.
+    y: u32,
+}
+
+impl Rule {
+    /// Determines if `update` is valid based on `self`.
+    fn is_valid(&self, update: &Update) -> bool {
+        let x_pos = update.iter().position(|&n| n == self.x);
+        let y_pos = update.iter().position(|&n| n == self.y);
+        match (x_pos, y_pos) {
+            (Some(x), Some(y)) => x < y,
+            _ => true,
+        }
+    }
+}
+
+impl FromStr for Rule {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s
+            .splitn(2, '|')
+            .map(str::parse::<u32>)
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Self {
+            x: parts[0],
+            y: parts[1],
+        })
+    }
+}
+
+/// A manual page update.
+#[derive(Debug)]
+struct Update(Vec<u32>);
+
+impl FromStr for Update {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s
+            .split(',')
+            .map(str::parse)
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Self(parts))
+    }
+}
+
+impl Update {
+    /// Determines if `self` is valid based on a series of `rules`.
+    fn is_valid(&self, rules: &[Rule]) -> bool {
+        rules.iter().all(|r| r.is_valid(self))
+    }
+
+    /// Returns the page number at `self`'s middle.
+    fn middle_page(&self) -> u32 {
+        self.0[self.0.len() / 2]
+    }
+
+    /// Returns an iterator over `self`'s pages.
+    fn iter(&self) -> Iter<'_, u32> {
+        self.0.iter()
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let dataset = aoc2024::get_dataset(&PathBuf::from(file!()), "input.txt");
+    let reader = BufReader::new(File::open(dataset)?);
+
+    let mut rules = vec![];
+    let mut updates = vec![];
+    for line in reader.lines() {
+        let line = line?;
+        if line.find('|').is_some() {
+            rules.push(Rule::from_str(&line)?);
+        } else if line.find(',').is_some() {
+            updates.push(Update::from_str(&line)?);
+        }
+    }
+
+    let total: u32 = updates
+        .iter()
+        .filter(|u| u.is_valid(&rules))
+        .map(Update::middle_page)
+        .sum();
+    println!("Sum of middle pages: {total}");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_test_rules() -> Vec<Rule> {
+        let data = vec![
+            "47|53", "97|13", "97|61", "97|47", "75|29", "61|13", "75|53", "29|13", "97|29",
+            "53|29", "61|53", "97|53", "61|29", "47|13", "75|47", "97|75", "47|61", "75|61",
+            "47|29", "75|13", "53|13",
+        ];
+        data.into_iter()
+            .map(Rule::from_str)
+            .collect::<Result<Vec<_>, _>>()
+            .unwrap()
+    }
+
+    fn get_test_updates() -> Vec<Update> {
+        let data = vec![
+            "75,47,61,53,29",
+            "97,61,53,29,13",
+            "75,29,13",
+            "75,97,47,61,53",
+            "61,13,29",
+            "97,13,75,29,47",
+        ];
+        data.into_iter()
+            .map(Update::from_str)
+            .collect::<Result<Vec<_>, _>>()
+            .unwrap()
+    }
+
+    #[test]
+    fn valid_updates_pass_verification() {
+        let rules = get_test_rules();
+        let updates = get_test_updates();
+
+        assert!(updates[0].is_valid(&rules));
+        assert!(updates[1].is_valid(&rules));
+        assert!(updates[2].is_valid(&rules));
+        assert!(!updates[3].is_valid(&rules));
+        assert!(!updates[4].is_valid(&rules));
+        assert!(!updates[5].is_valid(&rules));
+    }
+
+    #[test]
+    fn valid_updates_evaluate_to_correct_value() {
+        let rules = get_test_rules();
+        let updates: Vec<Update> = get_test_updates()
+            .into_iter()
+            .filter(|u| u.is_valid(&rules))
+            .collect();
+        let total: u32 = updates.iter().map(Update::middle_page).sum();
+
+        assert_eq!(total, 143);
+    }
+}
