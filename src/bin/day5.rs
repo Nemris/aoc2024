6b825e221612b2090cfef8f06d7f044a0d690cf6
Nemris <nemris@protonmail.com>
2024-12-14 01:58:39 +0100

    Improve performance by using Rust's sorting
    Consolidate all the printing rules in a HashMap, then do away with
    bubble sort by defining a custom comparator in order to let Rust handle
    the sorting of Updates.

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,47 +1,43 @@
 #![warn(clippy::pedantic)]
 
+use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::iter::Iterator;
 use std::num::ParseIntError;
 use std::path::PathBuf;
-use std::slice::Iter;
 use std::str::FromStr;
 
-/// A printing rule.
+/// Rules to sort page updates with.
+///
+/// Each page X is mapped to all the pages Y that must come after it.
 #[derive(Debug)]
-struct Rule {
-    /// This number must come before `y` in a valid `Update`.
-    x: u32,
-    /// This number must come after `x` in a valid `Update`.
-    y: u32,
-}
+struct PageRules(HashMap<u32, Vec<u32>>);
 
-impl Rule {
-    /// Determines if `update` is valid based on `self`.
-    fn is_valid(&self, update: &Update) -> bool {
-        let x_pos = update.iter().position(|&n| n == self.x);
-        let y_pos = update.iter().position(|&n| n == self.y);
-        match (x_pos, y_pos) {
-            (Some(x), Some(y)) => x < y,
-            _ => true,
-        }
+impl PageRules {
+    /// Returns a new `PageRules`.
+    fn new() -> Self {
+        Self(HashMap::new())
     }
-}
 
-impl FromStr for Rule {
-    type Err = ParseIntError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts = s
+    /// Parses a new `rule` and inserts it in `self`.
+    fn insert(&mut self, rule: &str) -> Result<(), ParseIntError> {
+        let parts = rule
             .splitn(2, '|')
             .map(str::parse::<u32>)
             .collect::<Result<Vec<_>, _>>()?;
+        self.0
+            .entry(parts[0])
+            .and_modify(|v| v.push(parts[1]))
+            .or_insert(vec![parts[1]]);
+        Ok(())
+    }
 
-        Ok(Self {
-            x: parts[0],
-            y: parts[1],
-        })
+    /// Returns the pages that must come after a page `x`.
+    fn get(&self, x: u32) -> Option<&Vec<u32>> {
+        self.0.get(&x)
     }
 }
 
@@ -63,81 +59,74 @@ impl FromStr for Update {
 }
 
 impl Update {
-    /// Determines if `self` is valid based on a series of `rules`.
-    fn is_valid(&self, rules: &[Rule]) -> bool {
-        rules.iter().all(|r| r.is_valid(self))
+    /// Sorts this update according to `rules`.
+    fn sort(&mut self, rules: &PageRules) {
+        self.0.sort_unstable_by(|x, y| match rules.get(*x) {
+            Some(ys) => {
+                if ys.contains(y) {
+                    Ordering::Less
+                } else {
+                    Ordering::Greater
+                }
+            }
+            None => Ordering::Equal,
+        });
     }
 
-    /// Applies all the `rules` until `self` is corrected.
-    fn apply_all(&mut self, rules: &[Rule]) {
-        // NOTE: very slow.
-        while let Some(r) = self.next_failed_rule(rules) {
-            self.apply(r);
-        }
-    }
-
-    /// Applies a single `rule` to correct `self`.
-    fn apply(&mut self, rule: &Rule) {
-        let x_pos = self
-            .0
-            .iter()
-            .position(|&n| n == rule.x)
-            .expect("the position should exist");
-        let y_pos = self
-            .0
-            .iter()
-            .position(|&n| n == rule.y)
-            .expect("the position should exist");
-        self.0.swap(x_pos, y_pos);
+    /// Checks if the pages in this update are sorted according to `rules`.
+    fn is_sorted(&self, rules: &PageRules) -> bool {
+        self.0.is_sorted_by(|x, y| match rules.get(*y) {
+            Some(xs) => !xs.contains(x),
+            None => true,
+        })
     }
 
     /// Returns the page number at `self`'s middle.
     fn middle_page(&self) -> u32 {
         self.0[self.0.len() / 2]
     }
+}
 
-    /// Returns the next rule that `self` fails to respect.
-    fn next_failed_rule<'r>(&self, rules: &'r [Rule]) -> Option<&'r Rule> {
-        rules.iter().find(|r| !r.is_valid(self))
-    }
-
-    /// Returns an iterator over `self`'s pages.
-    fn iter(&self) -> Iter<'_, u32> {
-        self.0.iter()
-    }
+/// Sums the middle pages of `Update`s in `iter`.
+fn sum_middle_pages<'a, I>(iter: I) -> u32
+where
+    I: Iterator<Item = &'a Update>,
+{
+    iter.map(Update::middle_page).sum()
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let dataset = aoc2024::get_dataset(&PathBuf::from(file!()), "input.txt");
     let reader = BufReader::new(File::open(dataset)?);
 
-    let mut rules = vec![];
+    let mut rules = PageRules::new();
     let mut updates = vec![];
     for line in reader.lines() {
         let line = line?;
         if line.find('|').is_some() {
-            rules.push(Rule::from_str(&line)?);
+            rules.insert(&line)?;
         } else if line.find(',').is_some() {
             updates.push(Update::from_str(&line)?);
         }
     }
 
-    let total: u32 = updates
-        .iter()
-        .filter(|u| u.is_valid(&rules))
-        .map(Update::middle_page)
-        .sum();
-    println!("Sum of middle pages: {total}");
+    let sorted = updates.iter().filter(|u| u.is_sorted(&rules));
+    println!("Sum of middle pages: {}", sum_middle_pages(sorted));
 
-    let mut bad_updates: Vec<Update> = updates
-        .into_iter()
-        .filter(|u| !u.is_valid(&rules))
-        .collect();
-    for u in &mut bad_updates {
-        u.apply_all(&rules);
-    }
-    let total: u32 = bad_updates.iter().map(Update::middle_page).sum();
-    println!("Sum of middle pages (fixed updates): {total}");
+    let sorted = {
+        let mut unsorted: Vec<_> = updates
+            .into_iter()
+            .filter(|u| !u.is_sorted(&rules))
+            .collect();
+        for u in &mut unsorted {
+            u.sort(&rules);
+        }
+        unsorted
+    };
+    println!(
+        "Sum of middle pages (after sorting): {}",
+        sum_middle_pages(sorted.iter())
+    );
 
     Ok(())
 }
@@ -146,16 +135,17 @@ fn main() -> Result<(), Box<dyn Error>> {
 mod tests {
     use super::*;
 
-    fn get_test_rules() -> Vec<Rule> {
+    fn get_test_rules() -> PageRules {
         let data = vec![
             "47|53", "97|13", "97|61", "97|47", "75|29", "61|13", "75|53", "29|13", "97|29",
             "53|29", "61|53", "97|53", "61|29", "47|13", "75|47", "97|75", "47|61", "75|61",
             "47|29", "75|13", "53|13",
         ];
-        data.into_iter()
-            .map(Rule::from_str)
-            .collect::<Result<Vec<_>, _>>()
-            .unwrap()
+        let mut pr = PageRules::new();
+        for r in data {
+            pr.insert(r).unwrap();
+        }
+        pr
     }
 
     fn get_test_updates() -> Vec<Update> {
@@ -178,12 +168,12 @@ mod tests {
         let rules = get_test_rules();
         let updates = get_test_updates();
 
-        assert!(updates[0].is_valid(&rules));
-        assert!(updates[1].is_valid(&rules));
-        assert!(updates[2].is_valid(&rules));
-        assert!(!updates[3].is_valid(&rules));
-        assert!(!updates[4].is_valid(&rules));
-        assert!(!updates[5].is_valid(&rules));
+        assert!(updates[0].is_sorted(&rules));
+        assert!(updates[1].is_sorted(&rules));
+        assert!(updates[2].is_sorted(&rules));
+        assert!(!updates[3].is_sorted(&rules));
+        assert!(!updates[4].is_sorted(&rules));
+        assert!(!updates[5].is_sorted(&rules));
     }
 
     #[test]
@@ -191,7 +181,7 @@ mod tests {
         let rules = get_test_rules();
         let updates: Vec<Update> = get_test_updates()
             .into_iter()
-            .filter(|u| u.is_valid(&rules))
+            .filter(|u| u.is_sorted(&rules))
             .collect();
         let total: u32 = updates.iter().map(Update::middle_page).sum();
 
@@ -203,11 +193,11 @@ mod tests {
         let rules = get_test_rules();
         let mut updates: Vec<Update> = get_test_updates()
             .into_iter()
-            .filter(|u| !u.is_valid(&rules))
+            .filter(|u| !u.is_sorted(&rules))
             .collect();
 
-        for u in updates.iter_mut() {
-            u.apply_all(&rules);
+        for u in &mut updates {
+            u.sort(&rules);
         }
         let total: u32 = updates.iter().map(Update::middle_page).sum();
         assert_eq!(total, 123);
